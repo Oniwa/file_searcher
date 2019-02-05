26fde1326559fb8b21f1d7e3130dceb6e5d9891f
John Barksdale <jdbarks@gmail.com>
2019-02-04 20:36:40 -0600

    Implemented getting user input

--- program.py
+++ program.py
@@ -1,21 +1,45 @@
+import os
+
+
 def main():
-    pass
+    print_header()
+    folder = get_folder_from_user()
+    if not folder:
+        print("Sorry we can't search that location.")
+        return
+
+    text = get_search_text_from_user()
+    if not text:
+        print("We can't search for nothing.")
+        return
+
+    search_folders(folder, text)
 
 
-def print_headers():
-    pass
+def print_header():
+    print('--------------------------------------')
+    print('            FILE SEARCH APP')
+    print('--------------------------------------')
 
 
 def get_folder_from_user():
-    pass
+    folder = input("What folder do you want to search? ")
+    if not folder or not folder.strip():
+        return None
+
+    if not os.path.isdir(folder):
+        return None
+
+    return os.path.abspath(folder)
 
 
 def get_search_text_from_user():
-    pass
+    text = input("What are you searching for [single phrases only]? ")
+    return text
 
 
-def search_file():
-    pass
+def search_folders(folder, text):
+    print(f'Would search {folder} for {text}')
 
 
 if __name__ == '__main__':
